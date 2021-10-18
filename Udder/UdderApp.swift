d2a94832ddac06fc468b6ff283b2de02898db7b0
Matt Mootz <mootz.matthew@gmail.com>
2021-10-17 21:04:42 -0700

    Initial Commit

---
+++ Udder/UdderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdderApp.swift
+//  Udder
+//
+//  Created by Matthew L Mootz on 10/17/21.
+//
+
+import SwiftUI
+
+@main
+struct UdderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
